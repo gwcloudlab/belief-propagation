960a723b5c8e42d47a25fd8e17e43136d4c9e049
mjost5v <mjt5v@virginia.edu>
2017-02-12 11:26:38 -0500

    Adding circular buffer

---
+++ src/constants.h
@@ -0,0 +1,19 @@
+/*
+ * constants.h
+ *
+ *  Created on: Feb 11, 2017
+ *      Author: ***REMOVED***
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+
+#define CHAR_BUFFER_SIZE 50
+
+#define MAX_STATES 2
+
+#define DEFAULT_STATE 1.0
+
+
+#endif /* CONSTANTS_H_ */
