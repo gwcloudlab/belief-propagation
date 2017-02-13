1186d0b36c3d3521b3f11ba4ad054c3c541678d1
mjost5v <mjt5v@virginia.edu>
2017-02-12 21:04:48 -0500

    Adding constants

---
+++ src/constants.h
@@ -0,0 +1,18 @@
+/*
+ * constants.h
+ *
+ *  Created on: Feb 12, 2017
+ *      Author: mjt5v
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+#define CHAR_BUFFER_SIZE 50
+
+#define DEFAULT_STATE 1.0
+
+#define MAX_STATES 2
+
+
+#endif /* CONSTANTS_H_ */
