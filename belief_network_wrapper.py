21f12f645b8b8472c6ea34712ba7f5d2715bce99
mjost5v <mjt5v@virginia.edu>
2017-03-26 14:06:12 -0400

    Adding more files

--- belief_network_wrapper.py
+++ belief_network_wrapper.py
@@ -2,12 +2,13 @@
 
 import subprocess
 
-i = 1E7
-while i < 1E9:
-    num_nodes = int(i)
-    num_arcs = 2 * num_nodes
-    file_name = 'src/benchmark_files/xml2/%d_%d.xml' % (num_nodes, num_arcs)
+i = 100000
+while i < 1000000:
+    if i != 100000 and i != 500000:
+        num_nodes = int(i)
+        num_arcs = 2 * num_nodes
+        file_name = 'src/benchmark_files/xml2/%d_%d.xml' % (num_nodes, num_arcs)
 
-    subprocess.call(['python', 'generate_belief_network.py', '--nodes', str(num_nodes), '--arcs', str(num_arcs), '--file', file_name])
+        subprocess.call(['python', 'generate_belief_network.py', '--nodes', str(num_nodes), '--arcs', str(num_arcs), '--file', file_name])
 
-    i *= 10
+    i += 100000
