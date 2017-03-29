aff78453463c367a3ede1f43f1c5a50c0af86ae5
mjost5v <mjt5v@virginia.edu>
2017-03-28 22:35:30 -0400

    Updating script Adding more benchmark files

--- submit_job.sh
+++ submit_job.sh
@@ -3,7 +3,7 @@
 #SBATCH -o benchmarks%j.out
 #SBATCH -e benchmarks%j.err
 # one hour timelimit
-#SBATCH --time 2:00:00
+#SBATCH --time 7:00:00
 # get gpu queue
 #SBATCH -p gpu
 # need 1 machine
@@ -13,29 +13,32 @@
 
 module load cuda/toolkit
 module load libxml2
-module load pgi
 module load cmake
 
 # build and run c benchmarks
 cd /home/trotsky/belief-propagation/src/c_benchmark
 cmake . -DCMAKE_BUILD_TYPE=Release
-make
+make clean && make
+rm *csv
 ./c_benchmark
 
 # build and run openmp benchmarks
 cd /home/trotsky/belief-propagation/src/openmp_benchmark
 cmake . -DCMAKE_BUILD_TYPE=Release
-make
+make clean && make
+rm *csv
 ./openmp_benchmark
 
 # build and run openacc benchmarks
 cd /home/trotsky/belief-propagation/src/openacc_benchmark
 cmake . -DCMAKE_BUILD_TYPE=Release
-make
+make clean && make
+rm *csv
 ./openacc_benchmark
 
 # build and run cuda benchmarks
 cd /home/trotsky/belief-propagation/src/cuda_benchmark
 cmake . -DCMAKE_BUILD_TYPE=Release
-make
+make clean && make
+rm *csv
 ./cuda_benchmark
