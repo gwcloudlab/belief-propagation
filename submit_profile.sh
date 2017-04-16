da66ca75ee8a28816817500ec9a4f3ced2d2db59
mjost5v <mjt5v@virginia.edu>
2017-04-15 22:58:40 -0400

    Updating scripts

--- submit_profile.sh
+++ submit_profile.sh
@@ -9,43 +9,29 @@
 # need 1 machine
 #SBATCH -N 1
 # name the job
-#SBATCH -J BeliefPropagationBenchmarks
+#SBATCH -J BeliefPropagationProfile
 
 module load cuda/toolkit
 module load libxml2
 module load cmake
-
-# build and run c benchmarks
-cd /home/***REMOVED***/belief-propagation/src/c_benchmark
-cmake . -DCMAKE_BUILD_TYPE=Release
-make clean && make
-rm *csv
-./c_benchmark
-
-# build and run openmp benchmarks
-cd /home/***REMOVED***/belief-propagation/src/openmp_benchmark
-cmake . -DCMAKE_BUILD_TYPE=Release
-make clean && make
-rm *csv
-./openmp_benchmark
 
 # build and run openacc benchmarks
 cd /home/***REMOVED***/belief-propagation/src/openacc_benchmark
 cmake . -DCMAKE_BUILD_TYPE=Release
 make clean && make
 rm *csv
-./openacc_benchmark
+nvprof --analysis-metrics -o openacc.nvprof ./openacc_benchmark
 
 # build and run cuda benchmarks
 cd /home/***REMOVED***/belief-propagation/src/cuda_benchmark
 cmake . -DCMAKE_BUILD_TYPE=Release
 make clean && make
 rm *csv
-./cuda_benchmark
+nvprof --analysis-metrics -o cuda.nvprof ./cuda_benchmark
 
 # build and run cuda kernels benchmarks
 cd /home/***REMOVED***/belief-propagation/src/cuda_benchmark_kernels
 cmake . -DCMAKE_BUILD_TYPE=Release
 make clean && make
 rm *csv
-./cuda_kernels_benchmark
+nvprof --analysis-metrics -o cuda_kernels.nvprof ./cuda_kernels_benchmark
