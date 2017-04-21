5f2ea19dfc1cd19163d04dda17deb0406817da40
mjost5v <mjt5v@virginia.edu>
2017-04-20 22:35:03 -0400

    Changing thread allocation

--- src/constants.h
+++ src/constants.h
@@ -20,9 +20,11 @@
 
 #define PRECISION 1E-3f
 
-#define BLOCK_SIZE 256
+#define BLOCK_SIZE 1024
 
-#define BLOCK_SIZE_2_D 16
+#define BLOCK_SIZE_2_D_X 64
+
+#define BLOCK_SIZE_2_D_Y 16
 
 #define MAX_NUM_NODES 1048
 
