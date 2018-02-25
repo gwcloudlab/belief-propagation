a543eaedacda348dcf01e37482cd27c10a114490
Michael Trotter <mjt5v@virginia.edu>
2018-02-25 14:41:17 -0500

    Prepping for new benchmarks

--- src/openacc_benchmark/non-loopy-belief-propagation.c
+++ src/openacc_benchmark/non-loopy-belief-propagation.c
@@ -18,14 +18,14 @@ int main(void) {
     run_test_belief_propagation_xml_file("../benchmark_files/xml2/800000_1600000.xml", out);
     run_test_belief_propagation_xml_file("../benchmark_files/xml2/1000000_2000000.xml", out);
      */
-    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/10_20.xml", out);
-    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/100_200.xml", out);
-    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/1000_2000.xml", out);
-    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/10000_20000.xml", out);
-    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/100000_200000.xml", out);
-    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/200000_400000.xml", out);
-    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/400000_800000.xml", out);
-    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/800000_1600000.xml", out);
+    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/10_20.bif.xml", out);
+    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/100_200.bif.xml", out);
+    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/1000_2000.bif.xml", out);
+    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/10000_20000.bif.xml", out);
+    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/100000_200000.bif.xml", out);
+    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/200000_400000.bif.xml", out);
+    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/400000_800000.bif.xml", out);
+    run_test_loopy_belief_propagation_edge_xml_file("/home/trotsky/gunrock_benchmark_files/800000_1600000.bif.xml", out);
 
     return 0;
 }
