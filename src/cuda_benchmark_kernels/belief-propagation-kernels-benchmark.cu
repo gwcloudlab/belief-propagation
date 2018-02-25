4d024eea827205a8cfd5303bf83426dfb0dd577a
Michael Trotter <mjt5v@virginia.edu>
2018-02-25 14:14:04 -0500

    Prepping for new benchmarks

--- src/cuda_benchmark_kernels/belief-propagation-kernels-benchmark.cu
+++ src/cuda_benchmark_kernels/belief-propagation-kernels-benchmark.cu
@@ -5,6 +5,7 @@ int main(void) {
     fprintf(out, "File Name,Propagation Type,Number of Nodes,Number of Edges,Diameter,Number of Iterations,BP Run Time(s)\n");
     fflush(out);
 
+    /*
     run_test_loopy_belief_propagation_xml_file_kernels("../benchmark_files/xml2/10_20.xml", out);
     run_test_loopy_belief_propagation_xml_file_kernels("../benchmark_files/xml2/100_200.xml", out);
     run_test_loopy_belief_propagation_xml_file_kernels("../benchmark_files/xml2/1000_2000.xml", out);
@@ -15,7 +16,15 @@ int main(void) {
     run_test_loopy_belief_propagation_xml_file_kernels("../benchmark_files/xml2/600000_1200000.xml", out);
     run_test_loopy_belief_propagation_xml_file_kernels("../benchmark_files/xml2/800000_1600000.xml", out);
     run_test_loopy_belief_propagation_xml_file_kernels("../benchmark_files/xml2/1000000_2000000.xml", out);
-
+*/
+    run_test_loopy_belief_propagation_xml_file_kernels("/home/trotsky/gunrock_benchmark_files/10_20.xml", out);
+    run_test_loopy_belief_propagation_xml_file_kernels("/home/trotsky/gunrock_benchmark_files/100_200.xml", out);
+    run_test_loopy_belief_propagation_xml_file_kernels("/home/trotsky/gunrock_benchmark_files/1000_2000.xml", out);
+    run_test_loopy_belief_propagation_xml_file_kernels("/home/trotsky/gunrock_benchmark_files/10000_20000.xml", out);
+    run_test_loopy_belief_propagation_xml_file_kernels("/home/trotsky/gunrock_benchmark_files/100000_200000.xml", out);
+    run_test_loopy_belief_propagation_xml_file_kernels("/home/trotsky/gunrock_benchmark_files/200000_400000.xml", out);
+    run_test_loopy_belief_propagation_xml_file_kernels("/home/trotsky/gunrock_benchmark_files/400000_800000.xml", out);
+    run_test_loopy_belief_propagation_xml_file_kernels("/home/trotsky/gunrock_benchmark_files/800000_1600000.xml", out);
 
     return 0;
 }
