50134fea102a68c7f5a7709f27f077b2749f96f1
Michael Trotter <mjt5v@virginia.edu>
2019-02-07 22:22:22 -0500

    Enabling multiple devices

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -24,9 +24,11 @@ cd "$ROOT_SRC_DIR/cmake-build-release/src/cuda_benchmark"
 ./cuda_edge_benchmark || true
 ./cuda_edge_streaming_benchmark || true
 #./cuda_edge_openmpi_benchmark || true
+./cuda_edge_multiple_devices_benchmark || true
 ./cuda_node_benchmark || true
 ./cuda_node_streaming_benchmark || true
 #./cuda_node_openmpi_benchmark || true
+./cuda_node_multiple_devices_benchmark || true
 
 # cuda-kernels benchmark
 cd "$ROOT_SRC_DIR/cmake-build-release/src/cuda_benchmark_kernels"
